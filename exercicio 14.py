b2ea117988343a29a7964266a84c4e6a2972bf9a
Pedro-Henrique-RL <pedroh130702@gmail.com>
2024-09-22 20:21:34 +0000

    Update exercicio 14.py

--- exercicio 14.py
+++ exercicio 14.py
@@ -1,11 +1,11 @@
-valor_emprestimo = float(input("Digite o valor do empréstimo: "))
-taxa_juros = float(input("Digite a taxa de juros mensal (em %): ")) / 100
-meses = int(input("Digite o número de meses para pagar: "))
-salario = float(input("Digite seu salário: "))
+soma = 0
+contador = 0
+while True:
+    nota = float(input("Digite uma nota (ou -1 para sair): "))
+    if nota == -1:
+        break
+    soma += nota
+    contador += 1
 
-prestacao_mensal = valor_emprestimo * (taxa_juros * (1 + taxa_juros) ** meses) / ((1 + taxa_juros) ** meses - 1)
-
-if prestacao_mensal > salario * 0.30:
-    print("Empréstimo negado. A prestação mensal é maior que 30% do seu salário.")
-else:
-    print("Empréstimo aprovado! A prestação mensal é:", prestacao_mensal)
+media = soma / contador if contador > 0 else 0
+print("A média das notas é:", media)
