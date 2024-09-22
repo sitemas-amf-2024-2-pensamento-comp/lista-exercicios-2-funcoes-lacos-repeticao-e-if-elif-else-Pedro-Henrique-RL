d170bfe93f3a5fb663ff96849727d23beda72a86
Pedro-Henrique-RL <pedroh130702@gmail.com>
2024-09-22 19:03:32 +0000

    Create exercicio 2.py

---
+++ exercicio 2.py
@@ -0,0 +1,10 @@
+tabuada = int(input("Digite o valor que deseja calcular a tabuada:"))
+
+multiplicador = 1
+
+while multiplicador <= 10:
+    resultado = tabuada * multiplicador
+    print(resultado)
+
+
+    multiplicador = multiplicador + 1
