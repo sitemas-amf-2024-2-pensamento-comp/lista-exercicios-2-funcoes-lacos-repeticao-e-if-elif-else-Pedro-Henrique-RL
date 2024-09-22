272087d9413fa52c4ea22375d5ce277f782dd1c4
Pedro-Henrique-RL <pedroh130702@gmail.com>
2024-09-22 20:22:38 +0000

    Create exercicio 16.py

---
+++ exercicio 16.py
@@ -0,0 +1,6 @@
+def calcular_area_retangulo(largura, altura):
+    return largura * altura
+
+largura = float(input("Digite a largura do retângulo: "))
+altura = float(input("Digite a altura do retângulo: "))
+print("A área do retângulo é:", calcular_area_retangulo(largura, altura))
